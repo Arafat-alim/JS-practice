4708ff1bc5a0e18eef34988d8d865c756252b820
Arafat <arafat97alim@gmail.com>
2021-08-14 18:36:45 +0530

    promise when user loggin.

---
+++ Promise/promise.js
@@ -0,0 +1,24 @@
+//!Promise
+
+//user LogginSYsytem
+var userLoggin = true;
+//create a promise object using new Promise constructor
+var promise = new Promise((resolve, reject) => {
+  //Promise is successfully
+  if (userLoggin) {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  } else {
+    reject();
+  }
+});
+
+promise
+  .then(() => {
+    //if my promise is success then
+    console.log("Promise is successfully filled");
+  })
+  .catch(() => {
+    console.log("Ooops successful nahi hua mera promse  :(");
+  });
