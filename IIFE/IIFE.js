8c9712489dd0df8bdff47aea500767a0777a5ea2
Arafat <arafat97alim@gmail.com>
2021-08-14 20:12:29 +0530

    IIFE files added with concept.

---
+++ IIFE/IIFE.js
@@ -0,0 +1,9 @@
+//! IIFE - Immediate Invoked Function Expression
+// syntax
+// (function(){...}())
+
+//code
+(function () {
+  console.log("hello boss");
+})();
+//call automatically, when the page is loaded
