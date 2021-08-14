22bd1b53d0650a997d28466cbf6eb7ad50dc90b2
Arafat <arafat97alim@gmail.com>
2021-08-14 19:24:51 +0530

    hoisitng in class.

---
+++ Hoisting/HoistingClass.js
@@ -0,0 +1,9 @@
+//! Two way to decalre a class
+
+const c = new Circle(); //we cont create circle object like this. because it is not define
+//! Class declaration (not support hoisitng)
+class Circle {
+  //dont support any hoisting concept
+}
+//! Class expression (We programmer dont use this much)3
+const Square = class Sq {};
