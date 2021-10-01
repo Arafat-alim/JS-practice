c6426a1f5bdeb6a58d9ba0749a3583f9d846396b
Arafat <arafat97alim@gmail.com>
2021-10-01 20:13:00 +0530

    ASI concept added.

---
+++ JS/ASI/code.js
@@ -0,0 +1,12 @@
+// ! ASI - Automatic semi - colon insertion.
+function List() {
+  return;
+  {
+    a: 5;
+  }
+}
+
+let obj = List();
+console.log(obj); // undefined
+
+//! must practice with the semi colon ending
