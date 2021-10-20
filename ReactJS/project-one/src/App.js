b04ddd8fe6dedc1b2795e9cb688b3beb1b394128
Arafat <arafat97alim@gmail.com>
2021-10-21 01:31:52 +0530

    Added prop,state and lazy initialization process concept.

--- ReactJS/project-one/src/App.js
+++ ReactJS/project-one/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
-import PersInfo from "./components/PersInfo";
+// import PersInfo from "./components/PersInfo";
+import CounterWithState2 from "./components/CounterWithState2";
+import LazyInitialization3 from "./components/LazyInitialization3";
 
 function App() {
   return (
     <div className="App">
-      <PersInfo name="Arafat" age="24" />
-      <PersInfo name="kashif" age="20" />
-      <PersInfo name="Mehmood" age="21" />
+      {/* <PersInfo name="Arafat" age="24" /> */}
+      <LazyInitialization3 />
     </div>
   );
 }
--- ReactJS/project-one/src/components/PersInfo.js
+++ ReactJS/project-one/src/components/PersInfo.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 function PersInfo(props) {
+  console.log(props); // list of object
+  //   ! Object destructuring
+  const { name, age } = props;
   return (
     <div className="prof-card">
-      <h2>Name: {props.name}</h2>
-      <h2>Age: {props.age}</h2>
+      <h2>Name: {name}</h2>
+      <h2>Age: {age}</h2>
     </div>
   );
 }
