91f19868b23b51b5b9b08e49439245f99f4c30c4
Arafat <arafat97alim@gmail.com>
2021-10-30 23:46:25 +0530

    React router basic package and basic program

--- ReactJS/project-one/src/App.js
+++ ReactJS/project-one/src/App.js
@@ -1,10 +1,32 @@
 import "./App.css";
-import ComponentWiseList from "./components/ListRendering/ComponentWiseList";
+import About from "./Router/About";
+import Home from "./Router/Home";
+import Product from "./Router/Product";
+import { Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <ComponentWiseList />
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/product">Product</Link>
+        </li>
+        <li>
+          <Link to="/about">About Us</Link>
+        </li>
+      </ul>
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/product">
+        <Product />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
     </div>
   );
 }
