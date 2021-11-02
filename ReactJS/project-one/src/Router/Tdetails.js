013c7fb4394b553b22701551ebe7ee15f3d7891f
Arafat <arafat97alim@gmail.com>
2021-11-02 22:45:48 +0530

    usehistory concept added.

--- ReactJS/project-one/src/Router/Tdetails.js
+++ ReactJS/project-one/src/Router/Tdetails.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { useParams } from "react-router";
+import { useParams, useHistory } from "react-router";
 
 function Tdetails() {
   const params = useParams();
+  const history = useHistory();
+  console.log(history);
   console.log(params);
   return (
     <div>
       <h1>Tutorial Details {params.title}</h1>
+      <button onClick={() => history.goBack()}>Go Back!</button>
+      <button onClick={() => history.push("/home")}>Home</button>
+      <button onClick={() => history.replace("/tutorial/regex")}>Regex</button>
     </div>
   );
 }
