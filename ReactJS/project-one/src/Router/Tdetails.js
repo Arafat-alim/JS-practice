e1828eaa7e7479db2b7998cf848069abafb833f3
Arafat <arafat97alim@gmail.com>
2021-11-02 23:11:18 +0530

    preventing navigation concept are added.

--- ReactJS/project-one/src/Router/Tdetails.js
+++ ReactJS/project-one/src/Router/Tdetails.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { useParams, useHistory } from "react-router";
+import { useParams, useHistory, Prompt } from "react-router";
 
 function Tdetails() {
   const params = useParams();
   const history = useHistory();
   console.log(history);
   console.log(params);
+  const dataEntered = false;
   return (
     <div>
+      {/* <Prompt when={!dataEntered} message="Do you want to leave this page" /> */}
+      <Prompt
+        when={!dataEntered}
+        message={(location, action) => {
+          if (location.pathname === "/about") {
+            return false;
+          } else {
+            return true;
+          }
+        }}
+      />
       <h1>Tutorial Details {params.title}</h1>
       <button onClick={() => history.goBack()}>Go Back!</button>
       <button onClick={() => history.push("/home")}>Home</button>
