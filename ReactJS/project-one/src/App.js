552267f5129c452829b05f5ace4175dc7fa95f1d
Arafat <arafat97alim@gmail.com>
2021-11-05 22:41:25 +0530

    setting up for the deleting part.

--- ReactJS/project-one/src/App.js
+++ ReactJS/project-one/src/App.js
@@ -2,13 +2,11 @@ import "./App.css";
 import Purchased from "./Redux/Purchased";
 import Cart from "./Redux/Cart";
 import Total from "./Redux/Total";
-import PurchasedCl from "./Redux/PurchasedCL";
 
 function App() {
   return (
     <div className="App">
       <Purchased />
-      <PurchasedCl />
       <Cart />
       <Total />
     </div>
