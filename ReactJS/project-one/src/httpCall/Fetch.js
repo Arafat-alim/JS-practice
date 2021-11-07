34e663d648a3fd5f03fc96874f0e26958d2097f0
Arafat <arafat97alim@gmail.com>
2021-11-08 01:27:12 +0530

    creating components and styling

--- ReactJS/project-one/src/httpCall/Fetch.js
+++ ReactJS/project-one/src/httpCall/Fetch.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import Todo from "./Component/Todo";
+import User from "./Component/User";
 
 const Fetch = () => {
   const [users, setUsers] = useState([]);
+  const [todos, setTodos] = useState([]);
   const fetchingUser = () => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
@@ -11,7 +14,8 @@ const Fetch = () => {
   const fetchTodos = () => {
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      // .then((data) => console.log(data));
+      .then((data) => setTodos(data));
   };
 
   return (
@@ -19,15 +23,13 @@ const Fetch = () => {
       <div className="topbar"></div>
       <button onClick={fetchTodos}>Todos</button>
       <button onClick={fetchingUser}>User</button>
+      <hr />
+      <h1>User Component</h1>
       {users.map((user, index) => {
-        return (
-          <div className="box1">
-            <h3>Id - {user.id}</h3>
-            <h3>Name - {user.name}</h3>
-            <h3>Username - {user.username}</h3>
-            <h3>Email - {user.email}</h3>
-          </div>
-        );
+        return <User user={user} key={index} />;
+      })}
+      {todos.map((todo, index) => {
+        return <Todo todo={todo} key={index} />;
       })}
     </div>
   );
